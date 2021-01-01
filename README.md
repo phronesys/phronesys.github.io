0d7adc4acb40459fcb2272eb4e4e3a2d8490dd9b
phronesys <daniel.dazarola@gmail.com>
2020-12-31 21:36:52 -0300

    nuxt yeah finally

---
+++ README.md
@@ -0,0 +1,5 @@
+# phronesys.github.io
+
+*[Home](https://phronesys.github.io/index)
+*[Projects](https://phronesys.github.io/projects)
+*[About](https://phronesys.github.io/about)
---
+++ pages/projects.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<div class="container">
+			<h1>projects</h1>
+		</div>
+		<div class="container">
+			<h2>
+				<nuxt-link to="/" class="button--grey">home</nuxt-link>
+			</h2>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
