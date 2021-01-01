42bc7034a195f0fc4fb1999a5069423e70e942e2
phronesys <daniel.dazarola@gmail.com>
2020-12-31 22:10:29 -0300

    saving the local

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # phronesys.github.io
-
-* [Home](https://phronesys.github.io/index)
-* [Projects](https://phronesys.github.io/projects)
-* [About](https://phronesys.github.io/about)
-* [About](https://phronesys.github.io/dist/about/index)
+## Local development 
+* [Home](https://phronesys.github.io/home)
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,46 @@
 export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+	// Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+	ssr: false,
 
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+	// Target (https://go.nuxtjs.dev/config-target)
+	target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'phronesys.github.io',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	router: {
+		base: '/home/',
+	},
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+	// Global page headers (https://go.nuxtjs.dev/config-head)
+	head: {
+		title: 'phronesys.github.io',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+	// Global CSS (https://go.nuxtjs.dev/config-css)
+	css: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	plugins: [],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+	// Auto import components (https://go.nuxtjs.dev/config-components)
+	components: true,
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+	buildModules: [
+		// https://go.nuxtjs.dev/eslint
+		'@nuxtjs/eslint-module',
+	],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-}
+	// Modules (https://go.nuxtjs.dev/config-modules)
+	modules: [],
+
+	// Build Configuration (https://go.nuxtjs.dev/config-build)
+	build: {},
+};
